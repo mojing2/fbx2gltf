555ab922a2820b8e4051a98afcd389b67f36d03f
Tracy Ma <1991md@gmail.com>
2017-08-23 16:26:03 +0800

    convert GLSL to SPIR-V

--- src/IO-glTF/gltfWriter-Shaders.cpp
+++ src/IO-glTF/gltfWriter-Shaders.cpp
@@ -21,8 +21,69 @@
 #include "StdAfx.h"
 #include "gltfWriter.h"
 #include "glslShader.h"
+#include <process.h>
 
 namespace _IOglTF_NS_ {
+
+	utility::string_t ConvertGlslToSpirv(utility::string_t& shader, utility::string_t& shaderName, glslShader::ShaderType stage)
+	{
+		wchar_t moduleFileName[1024];
+		int bytes = GetModuleFileName(NULL, moduleFileName, 1024);
+		assert(bytes != 0);
+		utility::string_t currentDir(moduleFileName);
+
+		// get shader file path
+		utility::string_t glslName;
+		if (stage == glslShader::ShaderType::Vertex)
+		{
+			glslName = shaderName + U(".vert");
+		}
+		else
+		{
+			glslName = shaderName + U(".frag");
+		}
+
+		utility::string_t inGlslPath = currentDir + U("\\..\\") + glslName;
+		utility::string_t outSpirvPath = currentDir + U("\\..\\") + shaderName;
+
+		// write shader to file
+		FILE* shaderFile = NULL;
+		_wfopen_s(&shaderFile, inGlslPath.c_str(), L"w");
+		assert(shaderFile != NULL);
+		fprintf_s(shaderFile, utility::conversions::to_utf8string(shader).c_str());
+		fflush(shaderFile);
+		fclose(shaderFile);
+
+		utility::string_t PvrTexToolExe(U("D:\\app\\vulkan_sdk_1.0.54.0\\Bin\\glslc.exe"));
+		utility::string_t cmd;
+		cmd += PvrTexToolExe;
+		cmd += U(" -std=310es ");
+		cmd += U(" -o ");
+		cmd += outSpirvPath;
+		cmd += U(" ");
+		cmd += inGlslPath;
+
+		wchar_t scriptPath[1024];
+		bytes = GetModuleFileName(NULL, scriptPath, 1024);
+		assert(bytes != 0);
+		wcscat_s(scriptPath, L"\\..\\glsl2spirv.bat");
+		FILE * f = NULL;
+		_wfopen_s(&f, scriptPath, L"w");
+		if (f == NULL)
+		{
+			ucout << "open bat file failed!" << std::endl;
+		}
+		fprintf_s(f, utility::conversions::to_utf8string(cmd).c_str());
+		fflush(f);
+		fclose(f);
+
+		auto result = _wspawnl(_P_WAIT, scriptPath, scriptPath, NULL);
+		assert(result != -1);
+
+		//image[name][U("uri")] = web::json::value::string(IOglTF::dataURI(outImagePath));
+
+		return outSpirvPath;
+	}
 
 bool gltfWriter::WriteShaders () {
 	//web::json::value buffer =web::json::value::object () ;
@@ -69,9 +130,19 @@ bool gltfWriter::WriteShaders () {
 		FbxString gltfFilename (utility::conversions::to_utf8string (_fileName).c_str ()) ;
 		
 		if ( GetIOSettings ()->GetBoolProp (IOSN_FBX_GLTF_EMBEDMEDIA, false) ) {
+			utility::string_t vertexShaderFilePath = ConvertGlslToSpirv(
+				tech.vertexShader().source(), 
+				vsName,
+				glslShader::ShaderType::Vertex
+			);
+			utility::string_t fragmentShaderFilePath = ConvertGlslToSpirv(
+				tech.fragmentShader().source(), 
+				fsName,
+				glslShader::ShaderType::Fragment
+			);
 			// data:[<mime type>][;charset=<charset>][;base64],<encoded data>
-			_json [U("shaders")] [vsName] [U("uri")] =web::json::value::string (IOglTF::dataURI (tech.vertexShader ().source (),0)) ;
-			_json [U("shaders")] [fsName] [U("uri")] =web::json::value::string (IOglTF::dataURI (tech.fragmentShader ().source (), 0)) ;
+			_json [U("shaders")] [vsName] [U("uri")] =web::json::value::string (IOglTF::dataURI ( vertexShaderFilePath )) ;
+			_json [U("shaders")] [fsName] [U("uri")] =web::json::value::string (IOglTF::dataURI ( fragmentShaderFilePath )) ;
 		} else {
 			utility::string_t vsFilename =_json [U("shaders")] [vsName] [U("uri")].as_string () ;
 			{
